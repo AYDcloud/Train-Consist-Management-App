aea2c72fcf8759972ca071c46aa313844d224af7
AYDcloud <ayushdalela4903@gmail.com>
2026-03-23 11:11:19 +0530

    done

--- App/src/TrainConsistApp.java
+++ App/src/TrainConsistApp.java
@@ -3,28 +3,9 @@ import java.util.ArrayList;
 public class TrainConsistApp {
 
     public static void main(String[] args) {
-
-        ArrayList<String> bogies = new ArrayList<>();
-
-        bogies.add("Sleeper");
-        bogies.add("AC Chair");
-        bogies.add("First Class");
-
-        System.out.println("After Adding Bogies:");
-        System.out.println(bogies);
-
-        bogies.remove("AC Chair");
-
-        System.out.println("\nAfter Removing AC Chair:");
-        System.out.println(bogies);
-
-        if (bogies.contains("Sleeper")) {
-            System.out.println("\nSleeper bogie exists in the train.");
-        } else {
-            System.out.println("\nSleeper bogie does not exist.");
-        }
-
-        System.out.println("\nFinal Bogie List:");
-        System.out.println(bogies);
+System.out.println("=========");
+        System.out.println("Train Consistent Management App");
+        List <String> trainConsist = newArrayList<>();
+        System.out.println("Train initialized");
     }
 }
