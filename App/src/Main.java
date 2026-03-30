991d7eab5d724e188339092e643a48b1ae53184a
AYDcloud <ayushdalela4903@gmail.com>
2026-03-30 10:19:24 +0530

    UC10

--- App/src/Main.java
+++ App/src/Main.java
@@ -1,41 +1,13 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
-class Bogie {
-    int id;
-    int capacity;
-    String type;
-
-    public Bogie(int id, int capacity, String type) {
-        this.id = id;
-        this.capacity = capacity;
-        this.type = type;
-    }
-
-    @Override
-    public String toString() {
-        return "Bogie ID: " + id + ", Capacity: " + capacity + ", Type: " + type;
-    }
-}
-
-public class TrainConsistApp {
+public class Main {
 
     public static void main(String[] args) {
 
-        List<Bogie> bogies = new ArrayList<>();
-        bogies.add(new Bogie(1, 50, "Passenger"));
-        bogies.add(new Bogie(2, 70, "Passenger"));
-        bogies.add(new Bogie(3, 40, "Cargo"));
-        bogies.add(new Bogie(4, 65, "Passenger"));
-        bogies.add(new Bogie(5, 80, "Cargo"));
+        System.out.println("=== Train Consist Management App ===");
 
-        Map<String, List<Bogie>> groupedBogies = bogies.stream()
-                .collect(Collectors.groupingBy(b -> b.type));
+        List<String> consist = new ArrayList<>();
 
-        System.out.println("Grouped Bogies:");
-        groupedBogies.forEach((type, list) -> {
-            System.out.println("\nType: " + type);
-            list.forEach(System.out::println);
-        });
+        System.out.println("Initial Bogie Count: " + consist.size());
     }
 }
--- App/src/TrainConsistApp.java
+++ App/src/TrainConsistApp.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.*;
+
+class Bogie {
+    int id;
+    int capacity;
+    String type;
+
+    public Bogie(int id, int capacity, String type) {
+        this.id = id;
+        this.capacity = capacity;
+        this.type = type;
+    }
+}
 
 public class TrainConsistApp {
+
+    public static void main(String[] args) {
+
+        List<Bogie> bogies = new ArrayList<>();
+        bogies.add(new Bogie(1, 50, "Passenger"));
+        bogies.add(new Bogie(2, 70, "Passenger"));
+        bogies.add(new Bogie(3, 40, "Cargo"));
+        bogies.add(new Bogie(4, 65, "Passenger"));
+        bogies.add(new Bogie(5, 80, "Cargo"));
+
+        int totalSeats = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
+
+        System.out.println("Total Seating Capacity: " + totalSeats);
+    }
 }
